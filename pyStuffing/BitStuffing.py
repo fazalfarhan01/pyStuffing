e2ee7c023edc2e2a2b5a7c9870d86d3ea9914656
fazalfarhan01 <fazal.farhan@gmail.com>
2021-05-13 14:30:34 +0530

    remodeled bit stuffing

--- pyStuffing/BitStuffing.py
+++ pyStuffing/BitStuffing.py
@@ -2,63 +2,34 @@ from typing import Counter
 from colorama import Fore
 # from .cls import cls
 
+
 class BitStuffing(object):
-    def __init__(self, sequence:list=None):
+    def __init__(self, sequence: list = None):
         if sequence == None:
             sequence = input("Enter the sequence (No Spaces): ")
             sequence = list(map(int, list(sequence)))
         self.sequence = sequence
-        self.bitFlag = "{}0, 1, 1, 1, 1, 1, 1, 0{}".format(Fore.LIGHTRED_EX, Fore.RESET)
+        self.bitFlag = "{}0, 1, 1, 1, 1, 1, 1, 0{}".format(
+            Fore.LIGHTRED_EX, Fore.RESET)
         self.count = 0
         self.stuffed = []
         self.stuffedColored = ""
         self.unStuffed = []
 
-        # self.startStuffing()
-        # self.startUnstuffing()
-    
     def startStuffing(self):
-        for bit in self.sequence:
-            if self.count == 5:
-                self.stuffed.append(0)
-                self.addColoredBit(0,True)
-                self.count = 0
+        self.stuffed = list(
+            map(int, list("".join(list(map(str, self.sequence))).replace("11111", "111110"))))
 
-            self.stuffed.append(bit)
-            self.addColoredBit(bit)
-
-            if bit == 1:
-                self.count += 1
-            else:
-                self.count = 0
-
-    def addColoredBit(self,bit:int, added:bool=False):
-        if len(self.stuffedColored) == 0:
-            self.stuffedColored = "["
-        else:
-            self.stuffedColored =  self.stuffedColored.replace("]",",")
-        if added:
-            self.stuffedColored += "{}{}{} ]".format(Fore.LIGHTGREEN_EX, bit, Fore.RESET)
-        else:
-            self.stuffedColored += "{} ]".format(bit)
-    
     def getStuffedColored(self) -> str:
-        return "[{}, {}, {}]".format(self.bitFlag, self.stuffedColored[1:-1], self.bitFlag)
+        return "[{}]".format(", ".join(list(map(str, self.stuffed))).replace("1, 1, 1, 1, 1, 0", "1, 1, 1, 1, 1, {}0{}".format(Fore.LIGHTRED_EX, Fore.RESET)))
 
     def startUnstuffing(self):
-        self.count = 0
-        for bit in self.stuffed:
-            if self.count == 5:
-                self.count = 0
-                continue
-            if bit == 1:
-                self.count += 1
-            self.unStuffed.append(bit)
-        
+        self.unStuffed = list(
+            map(int, list("".join(list(map(str, self.sequence))).replace("111110", "11111"))))
 
 
 if __name__ == "__main__":
-    stuff = BitStuffing([0,0,1,0,1,1,1,1,1,1,1,1,1,1,0])
+    stuff = BitStuffing([0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0])
     stuff.startStuffing()
     stuff.startUnstuffing()
     # cls()
