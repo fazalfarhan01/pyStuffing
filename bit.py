42ae13a5c80a5b028a78223c425f7e6a1a246656
fazalfarhan01 <fazal.farhan@gmail.com>
2021-05-13 14:41:56 +0530

    updating readme

--- bit.py
+++ bit.py
@@ -1,15 +1,15 @@
 from pyStuffing import BitStuffing
 from pyStuffing import clear
 
-# if __name__ == "__main__":
-stuff = BitStuffing()
-stuff.startStuffing() # START STUFFING
-stuff.startUnstuffing()
+if __name__ == "__main__":
+    stuff = BitStuffing()
+    stuff.startStuffing() # START STUFFING
+    stuff.startUnstuffing()
 
-clear() # CLEAR SCREEN
+    clear() # CLEAR SCREEN
 
-# PRINT SEQUENCES TO SCREEN
-print("Main Sequence:       {}".format(stuff.sequence))
-print("Stuffed Sequence:    {}".format(stuff.stuffed))
-print("Un-Stuffed Sequence: {}".format(stuff.unStuffed))
-print("Stuffed Sequence:    {}".format(stuff.getStuffedColored()))
+    # PRINT SEQUENCES TO SCREEN
+    print("Main Sequence:       {}".format(stuff.sequence))
+    print("Stuffed Sequence:    {}".format(stuff.stuffed))
+    print("Un-Stuffed Sequence: {}".format(stuff.unStuffed))
+    print("Stuffed Sequence:    {}".format(stuff.getStuffedColored()))
--- byte.py
+++ byte.py
@@ -2,14 +2,14 @@ from pyStuffing import ByteStuffing
 from pyStuffing import clear
 
 
-# if __name__ == "__main__":
-stuff = ByteStuffing(list("abcdefghijklmnopqrstuvwxyz".upper()), escape_character="E", flag_character="F")
-# stuff = ByteStuffing()
-stuff.startStuffing()
-stuff.startUnStuffing()
+if __name__ == "__main__":
+    stuff = ByteStuffing(list("abcdefghijklmnopqrstuvwxyz".upper()), escape_character="E", flag_character="F")
+    # stuff = ByteStuffing()
+    stuff.startStuffing()
+    stuff.startUnStuffing()
 
-clear()
+    clear()
 
-print("Sequence:  {}".format(stuff.sequence))
-print("Stuffed:   {}".format(stuff.stuffed))
-print("Un-stuffed: {}".format(stuff.unStuffed))
+    print("Sequence:  {}".format(stuff.sequence))
+    print("Stuffed:   {}".format(stuff.stuffed))
+    print("Un-stuffed: {}".format(stuff.unStuffed))
